7387674cb6befedcea30d1f20cf3d2bdc5c78205
victor <109164737+VictorrOG19@users.noreply.github.com>
2024-05-02 21:00:43 +0000

    New changes

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+// Metodo para ler os arquivos dos Comandos
+
 const fs = require("node:fs");
 const path = require("node:path");
 
-const dir = fs.readdirSync(path.join(__dirname, "./source"));
+const CmdArray = [];
+const source = path.join(__dirname, "./source");
+const dirs = fs.readdirSync(source);
 
-dir.forEach(i => {
-    let item = fs.readdir(i);
-    console.log(item);
+dirs.forEach(item => {
+    const allDirs = path.join(source, item);
+    
+    if (fs.statSync(allDirs).isDirectory()) {
+        
+        const itens = fs.readdirSync(allDirs);
+        itens.forEach(file => {
+            const pathForFiles = path.join(allDirs, file);
+
+            if (path.extname(file) === ".js") {
+                const content = fs.readFileSync(pathForFiles, "utf-8");
+                CmdArray.push(content);
+            }
+        });
+    }
 });
