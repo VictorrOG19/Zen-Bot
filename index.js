ae5e5f1c4d33fb1f830be70ffa358b35f5b7ae62
victor <109164737+VictorrOG19@users.noreply.github.com>
2024-05-03 19:40:30 +0000

    New changes

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const path = require("node:path");
 const CmdArray = [];
 const source = path.join(__dirname, "./source");
 const dirs = fs.readdirSync(source);
+let countItens = {"files": 0, "dirs": 0};
 
 dirs.forEach(item => {
     const allDirs = path.join(source, item);
-    
+    countItens.dirs++;
+
     if (fs.statSync(allDirs).isDirectory()) {
         
-        const itens = fs.readdirSync(allDirs);
+        itens = fs.readdirSync(allDirs);
         itens.forEach(file => {
             const pathForFiles = path.join(allDirs, file);
+            countItens.files++;
 
             if (path.extname(file) === ".js") {
                 const content = fs.readFileSync(pathForFiles, "utf-8");
@@ -23,3 +26,19 @@ dirs.forEach(item => {
         });
     }
 });
+
+const Eris = require("eris");
+const bot = new Eris.Client("");
+
+bot.on("ready", () => {
+    console.log(`${bot.user.username} está Pronto!`);
+});
+
+bot.connect();
+
+bot.slashCommands = new Eris.Collection();
+
+/*bot.application.slashCommands.set(CmdArray);*/
+console.log(`aaaaaa ${}`)
+
+/*${countItens.dirs} Diretório(s) encontrado(s).\n${countItens.files} Arquivo(s) encontrado(s).*/
--- source/adm/echo.js
+++ source/adm/echo.js
@@ -1,3 +1,12 @@
-{
-    name: "testeeeee"
+const Eris = require("eris");
+const Constants = Eris.Constants;
+
+module.exports = {
+    name: "teste",
+    description: "Apenas um comando de teste",
+    type: Constants.ApplicationCommandTypes.CHAT_INPUT,
+
+    run: async (bot, inter) => {
+        bot.createMessage("Um teste bem bolado")
+    }
 }
